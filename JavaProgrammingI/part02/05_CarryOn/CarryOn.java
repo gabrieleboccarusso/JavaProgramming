421e6b667c980fce60791e438ee79b14d24f2f88
Boccarusso <gabrieleboccarusso@gmail.com>
2022-03-24 18:12:53 +0100

    added exercise 5 of part 2

---
+++ JavaProgrammingI/part02/05_CarryOn/CarryOn.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+public class CarryOn {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String input = "a";
+        
+        while (!input.equals("no")) {
+            System.out.println("Shall we carry on?");
+            input = scanner.nextLine();
+        }
+    }
+}
