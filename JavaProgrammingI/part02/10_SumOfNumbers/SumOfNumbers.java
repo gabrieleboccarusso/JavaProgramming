fb691afbd98921f3faa2500474a401fdc3c69699
Boccarusso <gabrieleboccarusso@gmail.com>
2022-03-24 18:54:45 +0100

    added exercise 10 of part 2

---
+++ JavaProgrammingI/part02/10_SumOfNumbers/SumOfNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+	 
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        int sum = 0;
+        int input = 1;
+        
+        while (input != 0) {
+            System.out.println("Give a number:");
+            input = Integer.valueOf(scanner.nextLine());
+            sum += input;
+        }
+        
+        System.out.println("Sum of the numbers: "+sum);
+    }
+}
