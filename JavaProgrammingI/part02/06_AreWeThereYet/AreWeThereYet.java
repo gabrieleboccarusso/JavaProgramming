a1cd057ddc512af9b878f09ce9b85c09c56ebf99
Boccarusso <gabrieleboccarusso@gmail.com>
2022-03-24 18:13:59 +0100

    added exercise 6 of part 2

---
+++ JavaProgrammingI/part02/06_AreWeThereYet/AreWeThereYet.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+public class AreWeThereYet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int input = 1;
+        
+        while (input != 4) {
+            System.out.println("Give a number:");
+            input = Integer.valueOf(scanner.nextLine());
+        }
+    }
+}
