bd52883a79a327fadea24b5e9e48617d381269a5
Boccarusso <gabrieleboccarusso@gmail.com>
2022-04-29 10:03:45 +0200

    added exercise 5 of part 6

---
+++ JavaProgrammingI/part06/05_SantasWorkshop/Package.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Package {
+    private ArrayList<Gift> gifts;
+    
+    public Package() {
+        this.gifts = new ArrayList<Gift>();
+    }
+    
+    public void addGift(Gift gift) {
+        this.gifts.add(gift);
+    }
+    
+    public int totalWeight() {
+        int weight = 0;
+        
+        for(Gift gift : gifts) {
+            weight += gift.getWeight();
+        }
+        
+        return weight;
+    }
+}
