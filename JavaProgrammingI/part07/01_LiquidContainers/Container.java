cd3f843c2872bd1130eeb192939b3d718a1b227b
Boccarusso <gabrieleboccarusso@gmail.com>
2022-05-04 18:41:59 +0200

    added exercise 1 of part 7

---
+++ JavaProgrammingI/part07/01_LiquidContainers/Container.java
@@ -0,0 +1,43 @@
+public class Container {
+    int amount;
+    
+    public Container() {
+        this.amount = 0;
+    }
+    
+    public void add(int value) {
+        if (this.getFilled(value)) {
+            this.amount = 100;
+        } else if (value > 0) {
+            this.amount += value;
+        }
+    }
+    
+    public int getAmount() {
+        return this.amount;
+    }
+    
+    public void move(int value, Container container) {
+        if (value > this.amount) {
+            container.add(this.getAmount());
+            this.amount = 0;
+        } else {
+            container.add(value);
+            // use remove method
+            this.amount -= value;
+        }
+    }
+    
+    public boolean getFilled(int value) {
+        boolean flag = false;
+        if (this.amount + value > 100) {
+            flag = true;
+        }
+        
+        return flag;
+    }
+    
+    public void remove(int value) {    
+        this.amount = (this.amount - value < 0) ? 0 : this.amount - value;
+    }
+}
